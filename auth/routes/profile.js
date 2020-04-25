dad8a8f7dd4479f187c9bf2509b1d74f968a009b
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-04-25 00:20:16 -0400

    updated register profile route to handle  updating profiles should a user already have a profile within the db

--- auth/routes/profile.js
+++ auth/routes/profile.js
@@ -12,14 +12,24 @@ router.post("/", auth, async (req, res) => {
         const { firstName, lastName, avatar, github, cohort } = req.body;
         
 
-        let newProfile = await pool.query(
-            "INSERT into profile (aid, first_name, last_name, avatar, github, cohort) VALUES($1, $2, $3, $4, $5, $6) RETURNING *",
-            [req.user.id, firstName, lastName, avatar, github, cohort]
+        let existingProfile = await pool.query(
+            "SELECT * FROM profile WHERE aid=$1", [req.user.id]
         );
 
-        newProfile = newProfile.rows[0];
-        res.json(newProfile);
-        
+        if(existingProfile.rows.length){
+            let updatedProfile = pool.query("UPDATE profile SET first_name=$1, last_name=$2, avatar=$3, github=$4, cohort=$5 WHERE aid=$6 RETURNING *",
+            [firstName, lastName, avatar, github, cohort, req.user.id]);
+            return res.json((await updatedProfile).rows[0]);
+        }
+        else {
+            let newProfile = await pool.query(
+                "INSERT into profile (aid, first_name, last_name, avatar, github, cohort) VALUES($1, $2, $3, $4, $5, $6) RETURNING *",
+                [req.user.id, firstName, lastName, avatar, github, cohort]
+            );
+    
+            newProfile = newProfile.rows[0];
+            return res.json(newProfile);
+        } 
     } catch (error) {
        console.error(error.message);
        res.status(400).json({ errors: error }); 
