29465ed54e3c7a6525fdc0ca867069fcaae55366
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-04-26 21:17:45 -0400

    importer auth route inititialized the route

--- gateway/server.js
+++ gateway/server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const auth = require("./routes/auth");
 const PORT = process.env.PORT || 6000;
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth", auth);
 
-app.listen(PORT, () => console.log(`App is currently listening on port {PORT}`));
+app.listen(PORT, () => console.log(`App is currently listening on port ${PORT}`));
+
+// gateway >> auth service
+// frontend >> gateway >> authservice
