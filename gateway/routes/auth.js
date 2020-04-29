382830dca59a307a844539497766cc0acfdc83b8
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-04-28 20:49:41 -0400

    Complted registerProfile and getProfile functions for the api gateway.

--- gateway/routes/auth.js
+++ gateway/routes/auth.js
@@ -9,15 +9,20 @@ const { authServer } = require("../config/defaults");
 router.post("/", async (req, res) => {
   try {
     let response;
+    const token = req.header("x-auth-token");
+    let body = req.body.reqBody;
     switch (req.body.action) {
       case "registerUser":
-        response = await registerUser(req.body.reqBody);
+        response = await registerUser(body);
         break;
       case "loginUser":
-        response = await loginUser(req.body.reqBody);
+        response = await loginUser(body);
         break;
-    case "registerProfile":
-        response = await registerProfile(req.body.reqBody);
+      case "registerProfile":
+        response = await registerProfile(token, body);
+        break;
+      case "getProfile":
+        response = await getProfile(token);
         break;
       default:
         return res.status(404).json({ errors: { action: "Invalid request" } });
@@ -52,19 +57,30 @@ const loginUser = async (body) => {
   }
 };
 
-const registerProfile = async (req) => {
-    const token = req.header("x-auth-token");
+const registerProfile = async (token, body) => {
+  // decide if token is confirmed here on microservice
+  try {
+    const response = axios.post(`${authServer}/api/profiles/`, body, {
+      headers: { "x-auth-token": token },
+    });
+    return response;
+  } catch (error) {
+    console.error(error.response.data);
+    return error.response.data;
+  }
+};
 
-    // decide if token is confirmed here on microservice
-
-    try {
-        axios.post(`${authServer}/api/profiles`, req.body, { 
-            headers: { "x-auth-token": token }
-         });
-    } catch (error) {
-        console.error(error.response.data);
-        return error.response.data;
-    }
-}
+const getProfile = async (token) => {
+  // decide if token is confirmed here on microservice
+  try {
+    const response = axios.get(`${authServer}/api/profiles/self/`,  {
+      headers: { "x-auth-token": token },
+    });
+    return response;
+  } catch (error) {
+    console.error(error.response.data);
+    return error.response.data;
+  }
+};
 
 module.exports = router;
